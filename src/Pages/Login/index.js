64908b354c61d710ac30a5bdf505076df36aa01b
Marlon Strong <tsenane14@gmail.com>
2021-01-28 02:39:57 +0200

    added fonts

---
+++ src/Pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './LoginElements';
+
+function Login() {
+    return (
+        <>
+            <h1>Welcome to Beegran</h1>
+        </>
+    )
+}
+
+export default Login
